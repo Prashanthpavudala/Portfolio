2828ba83fef91ea0fa0f7ee02f0a4b76059c022e
Prashanth Reddy Pavudala <prashanthreddypavudala@gmail.com>
2025-09-29 16:01:09 -0400

    user image css, textual changes and resume update

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
         transition={{duration: 0.6, delay: 0.3}}
         className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'
         >
-          Hi! I'm Prashanth Reddy.Pavudala<Image src={assets.hand_icon} alt='' 
+          Hi! I'm Prashanth Reddy Pavudala<Image src={assets.hand_icon} alt='' 
         className='w-6'/>
       </motion.h3>
       
@@ -38,7 +38,7 @@ const Header = () => {
         whileInView={{opacity: 1}}
         transition={{duration: 0.6, delay: 0.7}}
         className='max-w-2xl mx-auto font-Ovo'>
-          Welcome to my profile! I'm a passionate and results-driven Full Stack Software Engineer with 3+ years of experience in developing scalable and high-performing applications. 
+          Welcome to my profile! I'm a passionate and results-driven Full Stack Software Engineer with 2+ years of experience in developing scalable and high-performing applications. 
           </motion.p>
       
       <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
@@ -57,7 +57,7 @@ const Header = () => {
           initial={{y: 30, opacity: 0}}
           whileInView={{y: 0, opacity: 1}}
           transition={{duration: 0.6, delay: 1.2}}
-          href='/Reddy_Resume.pdf' download
+          href='/Resume.pdf' download
           className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'
           >
             my resume
